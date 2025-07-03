fb5145ae236f527e8a57a27a92745c43166d89ac
vkrainyk <krainyk.viktoriia@gmail.com>
2025-07-03 09:02:32 +0300

    add updateContanctInfoTest Part2 - added fields verification

--- src/test/java/org/accountSettingsTests/AccountSettingsTests.java
+++ src/test/java/org/accountSettingsTests/AccountSettingsTests.java
@@ -3,7 +3,16 @@ package org.accountSettingsTests;
 import org.baseTest.BaseTest;
 import org.junit.Test;
 
+import static org.data.TestData.VALID_LOGIN_UI;
+
 public class AccountSettingsTests extends BaseTest {
+    final String NAME = "Viktoriia";
+    final String SURNAME = "Krainyk";
+    final String DATEOFBIRTH = "21";
+    final String MONTHOFBIRTH = "04";
+    final String YEAROFBIRTH = "1993";
+
+
     @Test
     public void updateContanctInfoTest() {
         pageProvider.getHomePage()
@@ -13,7 +22,12 @@ public class AccountSettingsTests extends BaseTest {
                 .checkIsRedirectToPersonalAccountPage()
                 .checkTabsNames()
                 .checkIsContactInfoTabIsActive()
-                ;
+                .checkValueInInputName(NAME)
+                .checkValueInInputSurname(SURNAME)
+                .checkValueInInputBirthday(String.format("%s.%s.%s", DATEOFBIRTH, MONTHOFBIRTH, YEAROFBIRTH))
+                .checkValueInInputEmail(VALID_LOGIN_UI)
+
+        ;
 
     }
 }
